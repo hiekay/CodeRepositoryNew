4fa23995f3e6f35adeea53b02a15faa4031c2153
Haoz <523280437@qq.com>
2017-03-15 10:24:39 +0800

    在Fragment中设置返回键的监听

---
+++ app/src/main/java/com/example/haoz/coderepositorynew/utils/setBackInFragment.java
@@ -0,0 +1,27 @@
+package com.example.haoz.coderepositorynew.utils;
+
+import android.view.KeyEvent;
+import android.view.View;
+
+/**
+ * Created by Haoz on 2017/3/15 0015.
+ */
+
+public class setBackInFragment {
+
+//   在onResume中调用
+//    private void getFocus() {
+//        getView().setFocusableInTouchMode(true);
+//        getView().requestFocus();
+//        getView().setOnKeyListener(new View.OnKeyListener() {
+//            @Override
+//            public boolean onKey(View v, int keyCode, KeyEvent event) {
+//                if(event.getAction() == KeyEvent.ACTION_UP && keyCode == KeyEvent.KEYCODE_BACK){
+//                    // .....
+//                    return true;
+//                }
+//                return false;
+//            }
+//        });
+//    }
+}
